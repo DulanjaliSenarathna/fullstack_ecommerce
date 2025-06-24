7a4b36534a358aecb169764920740060fc664e1a
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2025-06-24 13:10:37 +0800

    Header section done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './Pages/Home';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <>
-    <h1 className='text-danger'>Hello, World!</h1>
-    </>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' exact={true} element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Logo from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom'
+import CountryDropdown from '../CountryDropdown';
+import { IoIosSearch } from "react-icons/io";
+import Button from '@mui/material/Button';
+import { FaRegUser } from "react-icons/fa6";
+import { BsHandbag } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <>
+    <div className="headerWrapper">
+        <div className='top-strip bg-blue'>
+            <div className='container'>
+                <p className='mb-0 mt-0 text-center'>Due to the <b>COVID 19</b>  epidemic, orders may be processed with a slight delay</p>
+            </div>
+        </div> 
+        <header className='header'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='logoWrapper d-flex align-items-center col-sm-2'>
+                        <Link to={'/'}><img src={Logo} alt='logo'/></Link>
+                    </div>
+
+                    <div className='col-sm-10 d-flex align-items-center part2'>
+                        <CountryDropdown/>
+
+                        {/* Search Bar start here */}
+
+                        <div className='headerSearch ml-3 mr-3'>
+                            <input type='text' placeholder='Search for products...'/>
+                            <Button><IoIosSearch/></Button>   
+                        </div>
+
+                        {/* Search Bar ends here */}
+
+                        <div className='part3 d-flex align-items-center ml-auto'>
+                            <Button className='circle mr-3'><FaRegUser /></Button>
+                            <div className='ml-auto cartTab d-flex align-items-center'>
+                                <span className='price'>$3.29</span>
+                                <div className='position-relative ml-2'>
+                                    <Button className='circle'><BsHandbag /></Button>
+                                    <span className='count d-flex align-items-center justify-content-center'>1</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+    </>
+  )
+}
+
+export default Header
